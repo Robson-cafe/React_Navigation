e1ac268357c15cfd6a05b11761e706d9ee1e419f
Robson Silva <robsonveloso@hotmail.com>
2023-06-15 20:36:29 -0300

    #Commit aulas stack

--- navegacao/src/App.js
+++ navegacao/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'react-native-gesture-handler'
 import { SafeAreaView, Text } from 'react-native'
 //import TextoCentral from './components/TextoCentral'
 import TelaA from './views/TelaA'
--- navegacao/src/views/TelaA.js
+++ navegacao/src/views/TelaA.js
@@ -2,6 +2,7 @@ import React from 'react'
 import TextoCentral from '../components/TextoCentral'
 
 export default props => (
+    //props.navigation.navigate("TelaB") - testando navegação
     <TextoCentral corFundo='#e53935'>
         Tela A
     </TextoCentral>
